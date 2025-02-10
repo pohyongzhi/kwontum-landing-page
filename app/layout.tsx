577a7ee3abbe7cc0572b4b1095d9579c3d682e3c
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2025-02-10 18:27:24 +0800

    Edited next.config.mjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import type React from "react" // Import React
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import type React from "react"; // Import React
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Taekwondo Dojang",
-  description: "Welcome to our Taekwondo Dojang - Discover the Art of Korean Martial Arts",
-}
+    title: "Kwontum Taekwondo",
+    description:
+        "Welcome to our Kwontum Taekwondo - Discover the Art of Korean Martial Arts",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <link href="https://fonts.cdnfonts.com/css/dolce-vita" rel="stylesheet" />
-      </head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head>
+                {/* <link
+                    href="https://fonts.cdnfonts.com/css/dolce-vita"
+                    rel="stylesheet"
+                /> */}
+            </head>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
-
